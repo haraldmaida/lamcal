f46105a1a3cf9c51179da76fd743ba84d38a63e6
Harald Maida <harald.maida@innoave.com>
2018-08-24 23:15:38 +0200

    test optional support for failure crate

---
+++ tests/optional_failure_support.rs
@@ -0,0 +1,20 @@
+#![cfg(feature = "failure")]
+
+extern crate failure;
+
+extern crate lamcal;
+
+use failure::Error;
+
+use lamcal::{parse, Term};
+
+fn custom_parse(input: &str) -> Result<Term, Error> {
+    parse(input.chars()).map_err(From::from)
+}
+
+#[test]
+fn convert_parse_error_into_failure_error() {
+    let parsed = custom_parse("\\x.\\y y");
+
+    assert_eq!(parsed.unwrap_err().to_string(), "[LambdaBodyExpected]: expected a '.' character as start of the body of the lambda abstraction\n   1:7   \t   found:\tidentifier\n        \texpected:\tthe \'.\' character as start of the lambda body\n   hint: a lambda abstraction must contain a body, that is an expression following the '.' character\n");
+}
